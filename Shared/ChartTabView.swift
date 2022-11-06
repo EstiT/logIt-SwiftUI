f0cdc31f105530982f83a26f324bfd186ad62a23
Esti <estictweg@hotmail.com>
2022-11-06 17:34:15 +1030

    display date range, drag to pan chart (not correct amount)

--- Shared/ChartTabView.swift
+++ Shared/ChartTabView.swift
@@ -9,7 +9,38 @@ import CoreData
 import SwiftUI
 import Charts
 
+
+enum SwipeHVDirection: String {
+    case left, right, up, down, none
+}
+
+func detectDirection(value: DragGesture.Value) -> SwipeHVDirection {
+    if value.startLocation.x < value.location.x - 24 {
+        return .left
+    }
+    if value.startLocation.x > value.location.x + 24 {
+        return .right
+    }
+    if value.startLocation.y < value.location.y - 24 {
+        return .down
+    }
+    if value.startLocation.y > value.location.y + 24 {
+        return .up
+    }
+    return .none
+}
+
+
+var date: Date {
+    get {
+        let dateFormatter = ISO8601DateFormatter()
+        return dateFormatter.date(from: "2022-09-14T10:44:00+0000")!
+    }
+}
+
 struct ChartTabView: View {
+    
+    @State var domain: ClosedRange<Date> = date...Date()
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Session.date, ascending: true)],
         animation: .default)
@@ -17,12 +48,12 @@ struct ChartTabView: View {
     
     
     var body: some View {
-//        let strengthArr = sessions.map{Double($0.strengthRating)/5*100}
-//        let sessionArr = sessions.map{Double($0.sessionRating)/5*100}
+        
         NavigationView {
             GeometryReader { geometry in
                 
                 if sessions.count > 0 {
+                    Text(domain)
                     Chart {
                         ForEach(sessions) { session in
                             LineMark(
@@ -49,7 +80,7 @@ struct ChartTabView: View {
                             .foregroundStyle(by: .value("Strength", "Strength"))
                             .interpolationMethod(.catmullRom)
                             
-
+                            
                             PointMark(
                                 x: .value("Date", session.date!),
                                 y: .value("Strength Rating", session.strengthRating)
@@ -58,6 +89,32 @@ struct ChartTabView: View {
                             .foregroundStyle(session.injured ? .red : .green)
                         }
                     }
+                    .chartOverlay { proxy in
+                        GeometryReader { geometry in
+                            Rectangle().fill(.clear).contentShape(Rectangle())
+                                .gesture(
+                                    DragGesture()
+                                        .onChanged { value in
+                                            var delta = 0
+                                            
+                                            let direction = detectDirection(value: value)
+                                            if direction == .left {
+                                                delta = -2
+                                            } else if direction == .right {
+                                                delta = 2
+                                            }
+                                            
+                                            var dayComponent = DateComponents()
+                                            dayComponent.day = delta
+                                            
+                                            let newStart = Calendar.current.date(byAdding: dayComponent, to: domain.lowerBound)
+                                            let newEnd = Calendar.current.date(byAdding: dayComponent, to: domain.upperBound)
+                                            domain = newStart!...newEnd!
+                                        }
+                                )
+                        }
+                    }
+                    .chartXScale(domain: domain)
                 }
                 else {
                     HStack{
